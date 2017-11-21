97584a6ffc91cfddc2dc10d9b1d6f0b814084baa
Primovisto <ed@velabri.co>
2017-11-21 15:29:05 +0000

    Blog link added to index.html and registration work

--- Modapo_Marketplace/urls.py
+++ Modapo_Marketplace/urls.py
@@ -19,7 +19,7 @@ from django.views.static import serve
 from .settings import MEDIA_ROOT
 from marketplace import views
 from accounts import views as accounts_views
-from marketplace import views as marketplace_views
+from blog import views as blog_views
 
 urlpatterns = [
 
@@ -28,4 +28,10 @@ urlpatterns = [
     url(r'^media/(?P<path>.*)$', serve, {'document_root': MEDIA_ROOT}),
     url(r'^$', views.get_index, name='index'),
     url(r'^register/$', accounts_views.register, name='register'),
+    url(r'^profile/$', accounts_views.profile, name='profile'),
+    url(r'^login/$', accounts_views.login, name='login'),
+    url(r'^logout/$', accounts_views.logout, name='logout'),
+    url(r'^blog/$', blog_views.post_list, name='blog'),
+
+
 ]
